6ae1c8b0013712e307c441d6e560a9f4025b0f05
Пользователь <@-PC>
2023-05-13 14:22:57 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите температуру ");
+int t=int.Parse(Console.ReadLine());
+if (t == 20) Console.WriteLine("Можно выпустить кенгуру");
+else if (t < 20 && t >= -20) Console.WriteLine("Можно выпустить волков, медведей и пингвинов");
+else if (t > 20 && t <= 40) Console.WriteLine("Можно выпустить львов, жирафов и слонов");
+else Console.WriteLine("Никого нельзя выпускать!");
